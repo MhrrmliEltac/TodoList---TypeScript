744e0addf3c11bbbb8fb6c7e9eb5f587ccfcbd43
MhrrmliEltac <meherremlieltac14@gmail.com>
2024-08-24 03:24:57 +0400

    ToDo-App | TypeScript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+
 import "./App.css";
 import TodoCreate from "./Components/TodoCreate";
 import TodoList from "./Components/TodoList";
